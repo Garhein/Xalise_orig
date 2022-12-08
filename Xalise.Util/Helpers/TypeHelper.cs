5bd05adb237675afab0f76066961532c9fd533d9
Xavier VILLEMIN <xavier.villemin@gmail.com>
2022-12-08 20:33:41 +0100

    Xalise.Util : mise en place du helper sur les types de données

---
+++ Xalise.Util/Helpers/TypeHelper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Xalise.Util.Helpers
+{
+    /// <summary>
+    /// Fonctions utilitaires pour manipuler les types de données.
+    /// </summary>
+    public static class TypeHelper
+    {
+        /// <summary>
+        /// Récupère le nom du type d'une instance d'un objet.        
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <returns></returns>
+        public static string GetTypeName(Object obj)
+        {
+            if (obj == null)
+            {
+                throw new ArgumentNullException(nameof(obj));
+            }
+
+            return TypeHelper.GetTypeName(obj.GetType());
+        }
+
+        /// <summary>
+        /// Récupère le nom d'un type.
+        /// </summary>
+        /// <param name="type"></param>
+        /// <returns></returns>
+        public static string GetTypeName(Type type)
+        {
+            if (type == null)
+            {
+                throw new ArgumentNullException(nameof(type));
+            }
+
+            string className = type.FullName;
+            return className.Substring(className.LastIndexOf('.') + 1);
+        }
+    }
+}
