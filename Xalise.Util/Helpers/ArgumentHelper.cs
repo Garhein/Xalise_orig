ada9c66160d1f23e42cfd3331cd65af268ab482e
Xavier VILLEMIN <xavier.villemin@gmail.com>
2023-01-11 21:31:56 +0100

    [Xalise.Util] Mise en place du helper de validation des arguments de méthodes

---
+++ Xalise.Util/Helpers/ArgumentHelper.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Xalise.Util.Helpers
+{
+    /// <summary>
+    /// Fonctions utilitaires pour manipuler les arguments.
+    /// </summary>
+    public static class ArgumentHelper
+    {
+        #region NULL/Empty
+
+        /// <summary>
+        /// Vérifie si le paramètre est NULL.
+        /// </summary>
+        /// <param name="value">Valeur du paramètre.</param>
+        /// <param name="parameterName">Nom du paramètre.</param>
+        /// <exception cref="ArgumentNullException">Le paramètre est NULL.</exception>
+        public static void ThrowIfNull(object value, string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si le paramètre est NULL ou vide.
+        /// </summary>
+        /// <param name="value">Valeur du paramètre.</param>
+        /// <param name="parameterName">Nom du paramètre.</param>
+        /// <exception cref="ArgumentNullException">Le paramètre est NULL.</exception>
+        /// <exception cref="ArgumentException">Le paramètre est vide.</exception>
+        public static void ThrowIfNullOrEmpty(string value, string parameterName)
+        {
+            if (value == null)
+            {
+                throw new ArgumentNullException(parameterName);
+            }
+
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException($"Le paramètre '{parameterName}' ne peut pas être une chaîne vide.");
+            }
+        }
+
+        /// <summary>
+        /// Vérifie si le paramètre est NULL, vide ou composé uniquement d'espaces.
+        /// </summary>
+        /// <param name="value">Valeur du paramètre.</param>
+        /// <param name="parameterName">Nom du paramètre.</param>
+        /// <exception cref="ArgumentNullException">Le paramètre est NULL.</exception>
+        /// <exception cref="ArgumentException">Le paramètre est vide ou composé uniquement d'espaces.</exception>
+        public static void ThrowIfNullOrWhiteSpace(string value, string parameterName)
+        {
+            ArgumentHelper.ThrowIfNull(value, parameterName);
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentNullException($"Le paramètre '{parameterName}' ne pas être composé uniquement d'espaces.");
+            }
+        }
+
+        #endregion
+    }
+}
