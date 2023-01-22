41c9213d3d9a4340b287dfd10217a7614cbe7df3
Xavier VILLEMIN <xavier.villemin@gmail.com>
2023-01-22 10:52:05 +0100

    [Xalise.Util] Mise en place de NameOfHelper et des tests unitaires associés

---
+++ Xalise.Tests/NameOfHelperTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Xalise.Util.Helpers;
+
+namespace Xalise.Tests
+{
+    public class NameOfHelperTests
+    {
+        public class NameOfHelperDTO
+        {
+            public string PropDTO { get; set; }
+
+            public NameOfHelperDTO() { }
+        }
+
+        [Test]
+        public void NameOfProperty_WithStrAlias()
+        {
+            string ret = NameOfHelper.NameOfProperty<NameOfHelperDTO>(dto => dto.PropDTO, "alias");
+            Assert.AreEqual("alias.PropDTO", ret);
+        }
+
+        [Test]
+        public void NameOfProperty_WithoutStrAlias()
+        {
+            string ret = NameOfHelper.NameOfProperty<NameOfHelperDTO>(dto => dto.PropDTO);
+            Assert.AreEqual("PropDTO", ret);
+        }
+
+        [Test]
+        public void NameOfProperty_BoolWithoutAliasTrue()
+        {
+            string ret = NameOfHelper.NameOfProperty<NameOfHelperDTO>(dto => dto.PropDTO, true);
+            Assert.AreEqual("PropDTO", ret);
+        }
+
+        [Test]
+        public void NameOfProperty_BoolWithoutAliasFalse()
+        {
+            string ret = NameOfHelper.NameOfProperty<NameOfHelperDTO>(alias => alias.PropDTO, false);
+            Assert.AreEqual("alias.PropDTO", ret);
+        }
+    }
+}
---
+++ Xalise.Util/Helpers/NameOfHelper.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Linq.Expressions;
+using Xalise.Util.Extensions;
+
+namespace Xalise.Util.Helpers
+{
+    /// <summary>
+    /// Fonctions utilitaires pour récupérer le nom d'une variable, d'un type ou d'un membre.
+    /// </summary>
+    public static class NameOfHelper
+    {
+        /// <summary>
+        /// Permet d'obtenir le nom de chaîne simple avec son alias d'une variable, d'un type ou d'un membre.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name">Expression à évaluer.</param>
+        /// <param name="alias">Alias à inclure dans le résultlat.</param>
+        /// <returns>Si <paramref name="alias"/> renseigné renvoie "alias.NomPropriété", sinon renvoie "NomPropriété".</returns>
+        public static string NameOfProperty<T>(Expression<Func<T, object>> name, string alias = null)
+        {
+            if (!string.IsNullOrWhiteSpace(alias))
+            {
+                alias = $"{alias}.";
+            }
+
+            return $"{alias}{NameOfHelper.NameOf(name, true)}";
+        }
+
+        /// <summary>
+        /// Permet d'obtenir le nom de chaîne simple avec son alias d'une variable, d'un type ou d'un membre.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name">Expression à évaluer.</param>
+        /// <param name="sansAlias">Indique si l'alias doit être retiré ou inclu dans le résultat.</param>
+        /// <returns>Si <paramref name="sansAlias"/> renvoie "NomPropriété", sinon "alias.NomPropriété".</returns>
+        public static string NameOfProperty<T>(Expression<Func<T, object>> name, bool sansAlias)
+        {
+            return NameOfHelper.NameOf(name, sansAlias);
+        }
+
+        /// <summary>
+        /// Permet d'obtenir le nom de chaîne simple (non qualifié) d'une variable, d'un type ou d'un membre à partir de la propriété **nameof**.
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="name">Expression à évaluer.</param>
+        /// <param name="sansAlias">Indique si l'alias doit être retiré ou inclu dans le résultat.</param>
+        /// <returns>Si <paramref name="sansAlias"/> renvoie "NomPropriété", sinon "alias.NomPropriété".</returns>
+        public static string NameOf<T>(Expression<Func<T, object>> name, bool sansAlias = false)
+        {
+            Expression body             = name.Body;
+            MemberExpression membExpr   = null;
+
+            if (body.NodeType == ExpressionType.Convert || name.NodeType == ExpressionType.Convert)
+            {
+                membExpr = ((UnaryExpression)body).Operand as MemberExpression;
+            }
+            else if (name.NodeType == ExpressionType.Lambda)
+            {
+                membExpr = body as MemberExpression;
+            }
+            else if (name.NodeType == ExpressionType.MemberAccess)
+            {
+                membExpr = name as MemberExpression;
+            }
+
+            string valeur = membExpr.ToString();
+
+            if (sansAlias)
+            {
+                string nameAliasExpression = null;
+
+                if (name.Parameters != null && name.Parameters.IsNotEmpty())
+                {
+                    nameAliasExpression = name.Parameters[0].Name;
+                }
+
+                if (!string.IsNullOrWhiteSpace(nameAliasExpression))
+                {
+                    valeur = valeur.Remove(0, nameAliasExpression.Length + 1);
+                }
+            }
+
+            return valeur;
+        }
+    }
+}
