65ed7199ddc9a58afb48364a12c0e94a904e1bd8
Xavier VILLEMIN <xavier.villemin@gmail.com>
2023-08-13 11:51:35 +0200

    CollectionsExtension : amélioration (bis) des commentaires

--- Xalise.Core/Extensions/CollectionsExtension.cs
+++ Xalise.Core/Extensions/CollectionsExtension.cs
@@ -9,11 +9,11 @@
         /// Vérifie si la collection est vide.
         /// </summary>
         /// <remarks>
-        /// La collection est vide si elle est <c>null</c> ou ne contient aucune valeur.
+        /// La collection est vide si elle est <see langword="null"/> ou ne contient aucune valeur.
         /// </remarks>
         /// <typeparam name="T"></typeparam>
         /// <param name="source"></param>
-        /// <returns><c>true</c> si la collection est vide, sinon <c>false</c>.</returns>
+        /// <returns><see langword="true"/> si la collection est vide, sinon <see langword="false"/>.</returns>
         public static bool IsEmpty<T>(this IEnumerable<T> source)
         {
             return source == null || !source.Any();
@@ -23,11 +23,11 @@
         /// Vérifie si la collection n'est pas vide.
         /// </summary>
         /// <remarks>
-        /// La collection n'est pas vide si elle n'est pas <c>null</c> et contient au moins une valeur.
+        /// La collection n'est pas vide si elle n'est pas <see langword="null"/> et contient au moins une valeur.
         /// </remarks>
         /// <typeparam name="T"></typeparam>
         /// <param name="source"></param>
-        /// <returns><c>true</c> si la collection n'est pas vide, sinon <c>false</c>.</returns>
+        /// <returns><see langword="true"/> si la collection n'est pas vide, sinon <see langword="false"/>.</returns>
         public static bool IsNotEmpty<T>(this IEnumerable<T> source)
         {
             return !IsEmpty(source);
@@ -37,12 +37,12 @@
         /// Vérifie si la collection contient une valeur.
         /// </summary>
         /// <remarks>
-        /// La collection et la valeur recherchée ne doivent pas être <c>null</c>, et la collection doit contenir au moins une valeur.
+        /// La collection et <paramref name="value"/> ne doivent pas être <see langword="null"/>, et la collection doit contenir au moins une valeur.
         /// </remarks>
         /// <typeparam name="TValue"></typeparam>
         /// <param name="liste"></param>
         /// <param name="value">Valeur recherchée.</param>
-        /// <returns><c>true</c> si la collection contient la valeur, sinon <c>false</c>.</returns>
+        /// <returns><see langword="true"/> si la collection contient <paramref name="value"/>, sinon <see langword="false"/>.</returns>
         public static bool ExistAndContainsValue<TValue>(this IEnumerable<TValue> liste, TValue value)
         {
             return liste.IsNotEmpty() && value != null && liste.Contains(value);
@@ -52,13 +52,13 @@
         /// Vérifie si le dictionnaire contient une clé. 
         /// </summary>*
         /// <remarks>
-        /// Le dictionnaire et la clé recherchée ne doivent pas être <c>null</c>, et le dictionnaire doit contenir au moins une valeur.
+        /// Le dictionnaire et <paramref name="key"/> ne doivent pas être <see langword="null"/>, et le dictionnaire doit contenir au moins une valeur.
         /// </remarks>
         /// <typeparam name="TKey"></typeparam>
         /// <typeparam name="TValue"></typeparam>
         /// <param name="dictionary"></param>
         /// <param name="key">Clé recherchée.</param>
-        /// <returns><c>true</c> si le dictionnaire contient la clé, sinon <c>false</c>.</returns>
+        /// <returns><see langword="true"/> si le dictionnaire contient <paramref name="key"/>, sinon <see langword="false"/>.</returns>
         public static bool ExistAndContainsKey<TKey, TValue>(this IDictionary<TKey, TValue> dictionary, TKey key)
         {
             return dictionary.IsNotEmpty() && key != null && dictionary.ContainsKey(key);
