3c3b64d23449431342812f18d10caf13f1ff756b
Xavier VILLEMIN <xavier.villemin@gmail.com>
2023-08-15 10:53:22 +0200

    Tests unitaires de StringExtension

--- Xalise.Core/Extensions/StringExtension.cs
+++ Xalise.Core/Extensions/StringExtension.cs
@@ -6,15 +6,37 @@
     public static class StringExtension
     {
         /// <summary>
-        /// Retire l'ensemble des derniers caractères d'une chaîne si ceux-ci correspondent
-        /// au caractère indiqué en paramètre.
+        /// Indique si la chaîne est <see langword="null"/> ou vide (<c>""</c>).
         /// </summary>
+        /// <param name="src"></param>
+        /// <returns><see langword="true"/> si la chaîne est <see langword="null"/> ou vide (<c>""</c>), sinon <see langword="false"/>.</returns>
+        public static bool IsNullOrEmpty(this string src)
+        {
+            return string.IsNullOrEmpty(src);
+        }
+
+        /// <summary>
+        /// Indique si la chaîne est <see langword="null"/>, vide (<c>""</c>) ou composée uniquement d'espaces.
+        /// </summary>
+        /// <param name="src"></param>
+        /// <returns><see langword="true"/> si la chaîne est <see langword="null"/>, vide (<c>""</c>) ou composée uniquement d'espaces ; sinon <see langword="false"/>.</returns>
+        public static bool IsNullOrWhiteSpace(this string src)
+        {
+            return string.IsNullOrWhiteSpace(src);
+        }
+
+        /// <summary>
+        /// Retire l'ensemble des derniers caractères d'une chaîne si ceux-ci correspondent à <paramref name="charToRemove"/>.
+        /// </summary>
+        /// <remarks>
+        /// La chaîne d'origine est renvoyée si elle est <seealso cref="StringExtension.IsNullOrWhiteSpace(string)"/>.
+        /// </remarks>
         /// <param name="src">Chaîne d'origine.</param>
         /// <param name="charToRemove">Caractère à retirer.</param>
-        /// <returns>Chaîne nettoyée des caractères.</returns>
-        public static string RemoveCharsFromEnd(this string src, char charToRemove)
+        /// <returns>La chaîne nettoyée si elle n'est pas <seealso cref="IsNullOrWhiteSpace(string)"/>, sinon la chaîne sans modification. </returns>
+        public static string RemoveRepeatingCharsFromEnd(this string src, char charToRemove)
         {
-            if (string.IsNullOrWhiteSpace(src))
+            if (src.IsNullOrWhiteSpace())
             {
                 return src;
             }
@@ -48,18 +70,21 @@
                 return retVal;
             }
         }
-    
+
         /// <summary>
         /// Vérifie si les caractères présents dans la chaîne sont uniques.
         /// </summary>
-        /// <param name="src">Chaîne à vérifier.</param>
-        /// <returns>True si la chaîne ne contient pas de caractère en doublon, sinon False.</returns>
-        /// <exception cref="ArgumentException">Si <paramref name="src"/> est NULL ou composé uniquement d'espaces.</exception>
+        /// <remarks>
+        /// Une exception <see cref="ArgumentException"/> est levée si <paramref name="src"/> est NULL, vide ou composé uniquement d'espaces.
+        /// </remarks>
+        /// <param name="src"></param>
+        /// <returns><see langword="true"/> si les caractères  de <paramref name="src"/> sont uniques, sinon <see langword="false"/>.</returns>
+        /// <exception cref="ArgumentException"></exception>
         public static bool ContainsUniqueChars(this string src)
         {
-            if (string.IsNullOrWhiteSpace(src))
+            if (src.IsNullOrWhiteSpace())
             {
-                throw new ArgumentException("La chaîne à vérifier ne peut pas être NULL ou composée uniquement d'espaces.", nameof(src));
+                throw new ArgumentException("La chaîne à vérifier est NULL, vide ou composée uniquement d'espaces.", nameof(src));
             }
 
             char[] chars = src.ToCharArray();
---
+++ Xalise.Tests/Xalise.Core/Extensions/StringExtensionTests.cs
@@ -0,0 +1,200 @@
+﻿using NUnit.Framework;
+using System;
+using Xalise.Core.Extensions;
+
+namespace Xalise.Tests.Xalise.Core.Extensions
+{
+    public class StringExtensionTests
+    {
+        #region IsNullOrEmpty
+
+        /// <summary>
+        /// La chaîne est <see langword="null"/> => True.
+        /// </summary>
+        [Test]
+        public void IsNullOrEmpty_Null()
+        {
+            string strVal = null;
+            Assert.True(strVal.IsNullOrEmpty());
+        }
+
+        /// <summary>
+        /// La chaîne est <seealso cref="string.Empty"/> => True.
+        /// </summary>
+        [Test]
+        public void IsNullOrEmpty_Empty()
+        {
+            string strVal = string.Empty;
+            Assert.True(strVal.IsNullOrEmpty());
+        }
+
+        /// <summary>
+        /// La chaîne n'est pas <see langword="null"/> et contient du texte => False.
+        /// </summary>
+        [Test]
+        public void IsNullOrEmpty_NotEmpty()
+        {
+            string strVal = "abc";
+            Assert.False(strVal.IsNullOrEmpty());
+        }
+
+        #endregion
+
+        #region IsNullOrWhiteSpace
+
+        /// <summary>
+        /// La chaîne est <see langword="null"/> => True.
+        /// </summary>
+        [Test]
+        public void IsNullOrWhiteSpace_Null()
+        {
+            string strVal = null;
+            Assert.True(strVal.IsNullOrWhiteSpace());
+        }
+
+        /// <summary>
+        /// La chaîne est <seealso cref="string.Empty"/> => True.
+        /// </summary>
+        [Test]
+        public void IsNullOrWhiteSpace_Empty()
+        {
+            string strVal = string.Empty;
+            Assert.True(strVal.IsNullOrWhiteSpace());
+        }
+
+        /// <summary>
+        /// La chaîne est composée uniquement d'espaces blancs => True.
+        /// </summary>
+        [Test]
+        public void IsNullOrWhiteSpace_WhiteSpaces()
+        {
+            string strVal = "   ";
+            Assert.True(strVal.IsNullOrWhiteSpace());
+        }
+
+        /// <summary>
+        /// La chaîne n'est pas <see langword="null"/>, n'est pas vide et ne contient pas uniquement des espaces blancs.
+        /// </summary>
+        [Test]
+        public void IsNullOrWhiteSpace_NotEmpty()
+        {
+            string strVal = "abc";
+            Assert.False(strVal.IsNullOrWhiteSpace());
+        }
+
+        #endregion
+
+        #region RemoveRepeatingCharsFromEnd
+
+        /// <summary>
+        /// La chaîne est <see langword="null"/> => retour de la valeur d'origine.
+        /// </summary>
+        [Test]
+        public void RemoveRepeatingCharsFromEnd_Null()
+        {
+            string strVal = null;
+            string strRet = strVal.RemoveRepeatingCharsFromEnd('|');
+            Assert.True(strRet == null);
+        }
+
+        /// <summary>
+        /// La chaîne est <seealso cref="string.Empty"/> => retour de la valeur d'origine.
+        /// </summary>
+        [Test]
+        public void RemoveRepeatingCharsFromEnd_Empty()
+        {
+            string strVal = string.Empty;
+            string strRet = strVal.RemoveRepeatingCharsFromEnd('|');
+            Assert.True(strRet == "");
+        }
+
+        /// <summary>
+        /// La chaîne est composée uniquement d'espaces blancs => retour de la valeur d'origine.
+        /// </summary>
+        [Test]
+        public void RemoveRepeatingCharsFromEnd_WhiteSpaces()
+        {
+            string strVal = "   ";
+            string strRet = strVal.RemoveRepeatingCharsFromEnd('|');
+            Assert.True(strRet == "   ");
+        }
+
+        /// <summary>
+        /// Il n'y a pas de caractères répétitifs en fin de chaîne => retour de la valeur d'origine.
+        /// </summary>
+        [Test]
+        public void RemoveRepeatingCharsFromEnd_WithoutRepeatingChars()
+        {
+            string strVal = "abc|def|ghi|jkl";
+            string strRet = strVal.RemoveRepeatingCharsFromEnd('|');
+            Assert.True(strRet == "abc|def|ghi|jkl");
+        }
+
+        /// <summary>
+        /// Il y a des caractères répétitifs en fin de chaîne => retour de la chaîne nettoyée.
+        /// </summary>
+        [Test]
+        public void RemoveRepeatingCharsFromEnd_WithRepeatingChars()
+        {
+            string strVal = "abc|def|ghi|jkl|||||";
+            string strRet = strVal.RemoveRepeatingCharsFromEnd('|');
+            Assert.True(strRet == "abc|def|ghi|jkl");
+        }
+
+        #endregion
+
+        #region ContainsUniqueChars
+
+        /// <summary>
+        /// La chaîne est NULL => exception.
+        /// </summary>
+        [Test]
+        public void ContainsUniqueChars_Null()
+        {
+            string strVal = null;
+            Assert.Throws<ArgumentException>(() => strVal.ContainsUniqueChars());
+        }
+
+        /// <summary>
+        /// La chaîne est vide => exception.
+        /// </summary>
+        [Test]
+        public void ContainsUniqueChars_Empty()
+        {
+            string strVal = string.Empty;
+            Assert.Throws<ArgumentException>(() => strVal.ContainsUniqueChars());
+        }
+
+        /// <summary>
+        /// La chaîne contient uniquement des espaces => exception.
+        /// </summary>
+        [Test]
+        public void ContainsUniqueChars_WhiteSpacesOnly()
+        {
+            string strVal = "   ";
+            Assert.Throws<ArgumentException>(() => strVal.ContainsUniqueChars());
+        }
+
+        /// <summary>
+        /// La chaîne contient des caractères en double => False.
+        /// </summary>
+        [Test]
+        public void ContainsUniqueChars_RepeatingChars()
+        {
+            string strVal = "abcdefghijkladgj";
+            Assert.False(strVal.ContainsUniqueChars());
+        }
+
+        /// <summary>
+        /// La chaîne ne contient pas de caractère en double => True.
+        /// </summary>
+        [Test]
+        public void ContainsUniqueChars_UniqueChars()
+        {
+            string strVal = "abcdefghijkl";
+            Assert.True(strVal.ContainsUniqueChars());
+        }
+
+        #endregion
+    }
+}
