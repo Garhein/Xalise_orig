f8b16e45a063c932352a01d6adc752b9e3d37667
Xavier VILLEMIN <xavier.villemin@gmail.com>
2023-08-29 11:13:05 +0200

    Tests unitaires de ArgumentHelper

--- Xalise.Core/Helpers/ArgumentHelper.cs
+++ Xalise.Core/Helpers/ArgumentHelper.cs
@@ -1,4 +1,6 @@
-﻿namespace Xalise.Core.Helpers
+﻿using Xalise.Core.Extensions;
+
+namespace Xalise.Core.Helpers
 {
     /// <summary>
     /// Fonctions utilitaires pour manipuler les arguments.
@@ -8,11 +10,11 @@
         #region NULL/Empty
 
         /// <summary>
-        /// Vérifie si le paramètre est NULL.
+        /// Vérifie si <paramref name="value"/> est <seealso langword="null"/>.
         /// </summary>
         /// <param name="value">Valeur du paramètre.</param>
         /// <param name="parameterName">Nom du paramètre.</param>
-        /// <exception cref="ArgumentNullException">Le paramètre est NULL.</exception>
+        /// <exception cref="ArgumentNullException">Si <paramref name="value"/> paramètre est <seealso langword="null"/>.</exception>
         public static void ThrowIfNull(object value, string parameterName)
         {
             if (value == null)
@@ -22,17 +24,17 @@
         }
 
         /// <summary>
-        /// Vérifie si le paramètre est NULL ou vide.
+        /// Vérifie si <paramref name="value"/> est <seealso langword="null"/>.
         /// </summary>
         /// <param name="value">Valeur du paramètre.</param>
         /// <param name="parameterName">Nom du paramètre.</param>
-        /// <exception cref="ArgumentNullException">Le paramètre est NULL.</exception>
-        /// <exception cref="ArgumentException">Le paramètre est vide.</exception>
+        /// <exception cref="ArgumentNullException">Si <paramref name="value"/> est <seealso langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">Si <paramref name="value"/> est vide.</exception>
         public static void ThrowIfNullOrEmpty(string value, string parameterName)
         {
             ArgumentHelper.ThrowIfNull(value, parameterName);
 
-            if (string.IsNullOrEmpty(value))
+            if (value.IsNullOrEmpty())
             {
                 throw new ArgumentException($"Le paramètre '{parameterName}' ne peut pas être une chaîne vide.");
             }
@@ -43,13 +45,13 @@
         /// </summary>
         /// <param name="value">Valeur du paramètre.</param>
         /// <param name="parameterName">Nom du paramètre.</param>
-        /// <exception cref="ArgumentNullException">Le paramètre est NULL.</exception>
-        /// <exception cref="ArgumentException">Le paramètre est vide ou composé uniquement d'espaces.</exception>
+        /// <exception cref="ArgumentNullException">Si <paramref name="value"/> est <seealso langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">Si <paramref name="value"/> est vide ou composé uniquement d'espaces.</exception>
         public static void ThrowIfNullOrWhiteSpace(string value, string parameterName)
         {
             ArgumentHelper.ThrowIfNull(value, parameterName);
 
-            if (string.IsNullOrWhiteSpace(value))
+            if (value.IsNullOrWhiteSpace())
             {
                 throw new ArgumentException($"Le paramètre '{parameterName}' ne pas être composé uniquement d'espaces.");
             }
@@ -60,34 +62,36 @@
         #region Répertoires et fichiers
 
         /// <summary>
-        /// Vérifie si le répertoire n'existe pas.
+        /// Vérifie si le répertoire <paramref name="directory"/> n'existe pas.
         /// </summary>
         /// <param name="directory">Chemin du répertoire.</param>
-        /// <exception cref="DirectoryNotFoundException">Le répertoire n'a pas été trouvé.</exception>
+        /// <exception cref="ArgumentNullException">Si <paramref name="directory"/> est <seealso langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">Si <paramref name="directory"/> est vide ou composé uniquement d'espaces.</exception>
+        /// <exception cref="DirectoryNotFoundException">Si le répertoire <paramref name="directory"/> n'existe pas.</exception>
         public static void ThrowIfDirectoryNotFound(string directory)
         {
-            if (!string.IsNullOrWhiteSpace(directory))
+            ArgumentHelper.ThrowIfNullOrWhiteSpace(directory, nameof(directory));
+
+            if (!Directory.Exists(directory))
             {
-                if (!Directory.Exists(directory))
-                {
-                    throw new DirectoryNotFoundException($"Le répertoire <{directory}> n'a pas été trouvé.");
-                }
+                throw new DirectoryNotFoundException($"Le répertoire <{directory}> n'a pas été trouvé.");
             }
         }
 
         /// <summary>
-        /// Vérifier si le fichier n'existe pas.
+        /// Vérifier si le fichier <paramref name="filePath"/> n'existe pas.
         /// </summary>
         /// <param name="filePath">Chemin du fichier.</param>
-        /// <exception cref="FileNotFoundException">Le fichier n'a pas été trouvé.</exception>
+        /// <exception cref="ArgumentNullException">Si <paramref name="filePath"/> est <seealso langword="null"/>.</exception>
+        /// <exception cref="ArgumentException">Si <paramref name="filePath"/> est vide ou composé uniquement d'espaces.</exception>
+        /// <exception cref="FileNotFoundException">Si le fichier <paramref name="filePath"/> n'existe pas.</exception>
         public static void ThrowIfFileNotFound(string filePath)
         {
-            if (!string.IsNullOrWhiteSpace(filePath))
+            ArgumentHelper.ThrowIfNullOrWhiteSpace(filePath, nameof(filePath));
+
+            if (!File.Exists(filePath))
             {
-                if (!File.Exists(filePath))
-                {
-                    throw new FileNotFoundException($"Le fichier <{filePath}> n'a pas été trouvé.");
-                }
+                throw new FileNotFoundException($"Le fichier <{filePath}> n'a pas été trouvé.");
             }
         }
 
---
+++ Xalise.Tests/Xalise.Core/Helpers/ArgumentHelperTests.cs
@@ -0,0 +1,152 @@
+﻿using NUnit.Framework;
+using System;
+using System.IO;
+using Xalise.Core.Helpers;
+
+namespace Xalise.Tests.Xalise.Core.Helpers
+{
+    public class ArgumentHelperTests
+    {
+        #region NULL/Empty
+
+        /// <summary>
+        /// Objet <seealso langword="null"/> => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNull_IsNull()
+        {
+            object obj = null;
+            Assert.Throws<ArgumentNullException>(() => ArgumentHelper.ThrowIfNull(obj, nameof(obj)));
+        }
+
+        /// <summary>
+        /// Objet non <seealso langword="null"/> => exception non levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNull_IsNotNull()
+        {
+            object obj = new object();
+            Assert.DoesNotThrow(() => ArgumentHelper.ThrowIfNull(obj, nameof(obj)));
+        }
+
+        /// <summary>
+        /// Chaîne <seealso langword="null"/> => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrEmpty_IsNull()
+        {
+            string strVal = null;
+            Assert.Throws<ArgumentNullException>(() => ArgumentHelper.ThrowIfNullOrEmpty(strVal, nameof(strVal)));
+        }
+
+        /// <summary>
+        /// Chaîne non <seealso langword="null"/> mais vide => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrEmpty_IsEmpty()
+        {
+            string strVal = "";
+            Assert.Throws<ArgumentException>(() => ArgumentHelper.ThrowIfNullOrEmpty(strVal, nameof(strVal)));
+        }
+
+        /// <summary>
+        /// Chaîne non <seealso langword="null"/> et non vide => exception non levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrEmpty_IsValid()
+        {
+            string strVal = "Test";
+            Assert.DoesNotThrow(() => ArgumentHelper.ThrowIfNullOrEmpty(strVal, nameof(strVal)));
+        }
+
+        /// <summary>
+        /// Chaîne <seealso langword="null"/> => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrWhiteSpace_IsNull()
+        {
+            string strVal = null;
+            Assert.Throws<ArgumentNullException>(() => ArgumentHelper.ThrowIfNullOrWhiteSpace(strVal, nameof(strVal)));
+        }
+
+        /// <summary>
+        /// Chaîne non <seealso langword="null"/> mais vide => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrWhiteSpace_IsEmpty()
+        {
+            string strVal = "";
+            Assert.Throws<ArgumentException>(() => ArgumentHelper.ThrowIfNullOrWhiteSpace(strVal, nameof(strVal)));
+        }
+
+        /// <summary>
+        /// Chaîne non <seealso langword="null"/>, non vide mais composée uniquement d'espaces => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrWhiteSpace_OnlyWhiteSpaces()
+        {
+            string strVal = "   ";
+            Assert.Throws<ArgumentException>(() => ArgumentHelper.ThrowIfNullOrWhiteSpace(strVal, nameof(strVal)));
+        }
+
+        /// <summary>
+        /// Chaîne non <seealso langword="null"/>, non vide et pas composée uniquement d'espaces => exception non levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfNullOrWhiteSpace_IsValid()
+        {
+            string strVal = "Test";
+            Assert.DoesNotThrow(() => ArgumentHelper.ThrowIfNullOrWhiteSpace(strVal, nameof(strVal)));
+        }
+
+        #endregion
+
+        #region Répertoires et fichiers
+
+        /// <summary>
+        /// Le répertoire n'existe pas => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfDirectoryNotFound_NotFound()
+        {
+            string dirProject = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string dirTest    = Path.Combine(dirProject, "notFound");
+            Assert.Throws<DirectoryNotFoundException>(() => ArgumentHelper.ThrowIfDirectoryNotFound(dirTest));
+        }
+
+        /// <summary>
+        /// Le répertoire existe => exception non levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfDirectoryNotFound_Found()
+        {
+            string dirProject = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string dirBin     = Path.Combine(dirProject, "bin");
+            Assert.DoesNotThrow(() => ArgumentHelper.ThrowIfDirectoryNotFound(dirBin));
+        }
+
+        /// <summary>
+        /// Le fichier n'existe pas => exception levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfFileNotFound_NotFound()
+        {
+            string dirProject = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string dirFile    = Path.Combine(dirProject, "notFound.txt");
+            Assert.Throws<FileNotFoundException>(() => ArgumentHelper.ThrowIfFileNotFound(dirFile));
+        }
+
+        /// <summary>
+        /// Le fichier existe => exception non levée.
+        /// </summary>
+        [Test]
+        public void ThrowIfFileNotFound_Found()
+        {
+            string dirProject = Directory.GetParent(AppDomain.CurrentDomain.BaseDirectory).Parent.Parent.Parent.FullName;
+            string dirFile    = Path.Combine(dirProject, "Xalise.Tests.csproj");
+            Assert.DoesNotThrow(() => ArgumentHelper.ThrowIfFileNotFound(dirFile));
+        }
+
+        #endregion
+    }
+}
